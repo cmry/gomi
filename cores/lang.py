8eba5a683a637e2c2821f5813d27593b8fbc9f27
fazzeh <c.emmery@outlook.com>
2013-12-18 11:41:49 +0100

    18-12 tweak srv

--- cores/lang.py
+++ cores/lang.py
@@ -16,44 +16,44 @@ class Language:
 
     def jvt_lan(self, message, sender):
         m = message.split(' ')
-        jvt  = { 'whisky alfa':                 "Whiskey, India, Whiskey, Alfa \n <name> is groen als alfalfa ",
-                 'haakjes swag':                "Parkeer Alfa's man, wortel tussen kaakjes \n" +
+        jvt  = { 'whisky alfa':                 "Whiskey, India, Whiskey, Alfa, <name> is groen als alfalfa ",
+                 'haakjes swag':                "Parkeer Alfa's man, wortel tussen kaakjes " +
                                                 "Dumbell swag, zet <name> tussen haakjes",
                  'dood dimmen ' +
                  'sterf':                       "Zet me on hold, <name> doe dimmen, val even dood of dat soort dingen",
-                 'machine urine':               "Nou rest in peace, <name> zwem in urine \n" +
+                 'machine urine':               "Nou rest in peace, <name> zwem in urine " +
                                                 "Kleine shout-out naar de Machine",
-                 'moeder wiepen':               "En een kleine shout-out, nee, fuck it, doe een grote \n"
-                                                "naar <sname>'s moeder in d'r blootje \n Ze is niet echt mijn type " +
+                 'moeder wiepen':               "En een kleine shout-out, nee, fuck it, doe een grote "
+                                                "naar <sname>'s moeder in d'r blootje, Ze is niet echt mijn type " +
                                                 "hoor, maar ik zou het wel wiepen hoor",
                  'dom som ontelbaar':           "Jullie niggers zijn dommetjes, " +
-                                                "dus laat me afsluiten met een sommetje \n" +
+                                                "dus laat me afsluiten met een sommetje, " +
                                                 "Geen zorgen, ik ben snel klaar, <name> plus een is ontelbaar",
-                 'formule':                     "1, 2, 3, 5, 6 \n Dit is de fo, de fo, de formule",
-                 'een_twee':                    "Er is er geen een (2, 3), \n Zoals deze vier (5, 6) \n" +
-                                                "Dus geef <name> nog een \n Dit is de fo, de fo, de formule",
-                 'had_je':                      "Hier is er nog een (2, 3) \n Van deze vier (5, 6) \n" +
-                                                "Of had <name> al een? \n Dit is de fo, de fo, de formule",
-                 'niveau':                      "Gaat ie lekker <name>? U ziet het zo \n Het niveau gaat opnieuw" +
-                                                "omhoog \n voor all y'all Ivo Niehe volk",
+                 'formule':                     "1, 2, 3, 5, 6, Dit is de fo, de fo, de formule",
+                 'een_twee':                    "Er is er geen een (2, 3), Zoals deze vier (5, 6), " +
+                                                "Dus geef <name> nog een, Dit is de fo, de fo, de formule",
+                 'had_je':                      "Hier is er nog een (2, 3), Van deze vier (5, 6), " +
+                                                "Of had <name> al een? Dit is de fo, de fo, de formule",
+                 'niveau':                      "Gaat ie lekker <name>? U ziet het zo,  Het niveau gaat opnieuw" +
+                                                "omhoog, voor all y'all Ivo Niehe volk",
                  'kunstschaatsen outfit ' +
-                 'schaatsen eist':              "<name> eist dingen als kunstschaatsen \n Stijl sierlijk als " +
-                                                "kunstschaatsen \n Outfit strak als kunstschaatsen \n Laat me effe " +
+                 'schaatsen eist':              "<name> eist dingen als kunstschaatsen, Stijl sierlijk als " +
+                                                "kunstschaatsen, Outfit strak als kunstschaatsen, Laat me effe " +
                                                 "in je uh schaatsen",
                  'exemplaar hilarisch ' +
-                 'kanarie':                     "Exemplarisch, formularisch \n Een hoop lol, de vorm hilarisch \n" +
+                 'kanarie':                     "Exemplarisch, formularisch, Een hoop lol, de vorm hilarisch, " +
                                                 "Gele wervelwind, de storm kanarisch",
                  'banaan':                      "Banaan in <name>'s oor hoort het commentaar niet",
-                 'jas das pinpas' +
-                 'drankje':                     "Jasje, dasje, pinpasje \n <name>'s drankje, in het handje \n" +
-                                                "Buitenlandje, binnenlandje \n Pimpendansje, in het Fransje",
+                 'jas pinpas' +
+                 'drankje':                     "Jasje, dasje, pinpasje, <name>'s drankje, in het handje, " +
+                                                "Buitenlandje, binnenlandje, Pimpendansje, in het Fransje",
                  'frans':                       "In het Fransje, oui oui",
-                 'win':                         "<name>, goddammit nummer 1 \n Neem iedereen in mijn slipstream mee",
-                 'element systeem':             "Het hele periodieke systeem \n heeft niks op het element <name>ley",
+                 'win':                         "<name>, goddammit nummer 1, Neem iedereen in mijn slipstream mee",
+                 'element systeem':             "Het hele periodieke systeem, heeft niks op het element <name>ley",
                  'hardgaan kwardraat' +
-                 'achtbaan':                    "Warpspeed hard gaan, <name> in het kwadraat, \n" +
+                 'achtbaan':                    "Warpspeed hard gaan, <name> in het kwadraat, " +
                                                 "op in de nacht gaan, samen in een achtbaan",
-                 'oplossing':                   "De oplossing van de formule? \n <name> zonder scrupules"
+                 'oplossing':                   "De oplossing van de formule? <name> zonder scrupules"
         }
 
         for w in m:
