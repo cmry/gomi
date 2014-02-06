730bb40b813c4cd1498fddc00d7259146b002ae4
fazzeh <c.emmery@outlook.com>
2014-02-06 15:14:21 +0100

    Fix TODO's added.

--- ftuk.py
+++ ftuk.py
@@ -60,6 +60,18 @@ class Ftuk:
 
          Just plug stable class dailies in here, don't forget to add args."""
 
+        # TODO: get the actual wsj10 file as gold standard
+        # TODO: check for the actual amount of tags and n-grams (2/3) in gold file
+        # TODO: compare these numbers with the actual statistics
+
+        # TODO: from the top 5 n-grams (2/3), if correct output >> STRUC and GS
+        #       sentence, validation by eye. Randomly output 100 sentence pairs.
+
+        # TODO: check how the total structs GS and ST compare in number
+        # TODO: check if the branch & misc errors are still in the final dict
+
+        # TODO: fix the n-gram stuff
+
         if args['freq']:
             tagger.main(args)
         elif args['fix']:
