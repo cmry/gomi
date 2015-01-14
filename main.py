145a53d3eac75702407adb33caeb69c55470babe
fazzeh <c.emmery@outlook.com>
2015-01-14 12:34:12 +0100

    lunch changes

--- main.py
+++ main.py
@@ -29,9 +29,11 @@ btab = '''
 \\begin{tabular}{---al---}
 ---ta--- '''
 
-ctab = '''        
+ctab = '''
 \\end{tabular} }
 \\end{table} '''
+
+bib = {}
 
 inil = {'Van ': 'van ', 'Den ': 'den ', 'Der ': 'der ', 'De ': 'de ',
         'Het ': 'het ', "'T ": "'t ", 'Des ': 'des ', 'Op ': 'op '}
@@ -43,7 +45,7 @@ cust = {'Veronique Hoste':                  u"Hoste, V\u00E9ronique",
         'Antal van Den Bosch':              u'van Den Bosch, Antal'}
 
 
-def sanitize(texs):
+def sanitize(texs, istex=None):
     """
     This function is used to escape any colliding characters with
     LaTeX, correct stupid unicode characters, and do a clean-up on
@@ -59,22 +61,25 @@ def sanitize(texs):
     texs = texs.replace("\%", "%")
     texs = texs.replace("%", "\%")
     texs = texs.replace("_", "\_")
-    texs = texs.replace("}", "\}")
-    texs = texs.replace("{", "\{")
     # crappy unicode stuff
+    #texs = texs.replace('–', '-')
     texs = texs.replace(u'\u00A0', " ")  # screw you mac
     texs = texs.replace(u'\u00AD', "")   # and some more
-    texs = texs.replace('\n', ' ')
     # user errors
     texs = texs.replace('htttp', 'http')
     # system errors
     texs = texs.replace("&amp;", "&")
     texs = texs.replace("amp;", '')
-    texs = token_url(texs)
+    # text only stuff
+    if not istex:
+        texs = texs.replace('\n', ' ')
+        texs = texs.replace("}", "\}")
+        texs = texs.replace("{", "\{")
+        texs = token_url(texs, True)
     return texs
 
 
-def token_url(text):
+def token_url(text, fn=False):
     """
     This function is used to recognize URLs in text and format them
     so that they will be placed in a footnote underneath the abstract.
@@ -83,13 +88,14 @@ def token_url(text):
     :param text: str unicode text
     :return: str with footnoted URLs
     """
+    # TODO: smarter handling of foonote
     urls = findall(' ?(?:http|ftp|https):\/\/[a-z./0-9%].*(?:\.[a-z]{2,5}|\/)(?: |\n|\.|\)|,|$)', text)
     if urls:
         for u in urls:
             lm = u[len(u)-1:]  # try to trim stuff before the URL
             u = u[:-1] if (lm == '.' or lm == ')' or lm == ',') else u  # trim
-            text = text.replace(u, "\\footnote{\\url{"+u+"}}")  # insert footnote
-    burls = findall('(?:,|\.)\\\\footnote\{\\\\url\{.*\}\}', text)
+            text = text.replace(u, ("\\footnote{" if fn else '') + " \\url{"+u+"}" + ("}" if fn else ''))  # insert footnote
+    burls = findall('(?:,|\.)' + ('\\\\footnote\{' if fn else '') + ' \\\\url\{.*\}' + ('\}' if fn else ''), text)
     if burls:  # if , or . before footnote, switch them
         for bu in burls:
             text = text.replace(bu, bu[1:]+bu[0])
@@ -105,24 +111,34 @@ def check_double_ref(text):
     return textii
 
 
-def format_text(text):
+def format_ref(refs, label):
+    refs = refs.split('\n')
+    refs = list(' '.join(refs)) if 'Ramage' in ' '.join(refs) else refs  # custom!
+    for n in refs:
+        if len(n) > 10:
+            n = n[1:] if n.startswith(' ') else n
+            n = token_url(n)
+            bib[(hash(n), label)] = n
+
+
+def format_text(text, title):
     ref = findall(r'\n(?:[Rr]eference)|(?:REFERENCE)[sS]?[:]?', text)
     brf = findall(r'\n\[[0-9]\]', text)
-    tref = ''
+    label = 'tit:' + str(hash(title))
     if brf or ref:
         tl = text.split((brf[-1:] if (brf and not ref) else ref[-1:])[0])
-        text, ref = tl[0], tl[1]
-        # TODO: this shit does not work yet
-        # tref = '\\begin{thebibliography}{10}\n'+('\n'.join(['\\bibitem{'+n+'}' for n in ref.split('\n')]))+'\\end{thebibliography}\n'
-    return text+'\n'+tref
+        text, ref = tl[0], sanitize(tl[1], True)
+        format_ref(ref, label)
+    return sanitize(text)+'\n'+'\\label{'+label+'}'
 
 
 def format_toc(tit, name_l):
-    # TODO: these are the ugliest two one-liners in existance, please fix them to something 
-    # readable; first converts tuples to strings, checks in dict, why not have either tuples 
-    # in dict or just join to string and then do the second function for adding, because 
+    # TODO: these are the ugliest two one-liners in existance, please fix them to something
+    # readable; first converts tuples to strings, checks in dict, why not have either tuples
+    # in dict or just join to string and then do the second function for adding, because
     # that converts to a string anyway. First one breaks everything, great.
-    # name_l = [(i.split()[0], i.split()[1]) for i in [n[0]+' '+n[1] if n[0]+' '+n[1] not in cust else cust[n[0]+' '+n[1]] for n in name_l]]
+    # name_l = [(i.split()[0], i.split()[1]) for i in [n[0]+' '+n[1]
+    # if n[0]+' '+n[1] not in cust else cust[n[0]+' '+n[1]] for n in name_l]]
     aut = ', '.join([('\\newline ' if (name_l.index(n) == 4 and len(', '.join([n[0]+' '+n[1] for n in name_l[:3]])) < 72) else '') + n[0]+' '+n[1] for n in name_l])
     # --------------------------------------------------------------------------------------
     aut = aut.split(' \\newline')
@@ -153,13 +169,14 @@ def check_prio(tl):
 
 def format_title(tit):
     global titl
+    # TODO: { } is not handled yet
     if 62 < len(tit) < 96:
         tl = tit.split()
         tl.insert(len(tl)/2 if not check_prio(tl) else check_prio(tl), '\\\\')
         for word in list(set(tl) & set(titl)):
             if tl.index(word)-tl.index('\\\\') == 1 and ':' not in tl[tl.index(word)-2]: # page 41
                 a, b = tl.index(word), tl.index('\\\\')
-                tl[a], tl[b] = tl[b], tl[a] 
+                tl[a], tl[b] = tl[b], tl[a]
         tit = ' '.join(tl)
     if tit[-1:] == '.':
         tit = tit[:-1]
@@ -212,6 +229,16 @@ def format_table(namel, afil, maill):
     return '\n'.join(ltab)
 
 
+def get_refs():
+    global bib
+    bib = OrderedDict(sorted(bib.items(), key=lambda (k, v): v))
+    bibt = '\\chapter{Bibliography} \n\\begin{itemize} \n'
+    for tup, cit in bib.iteritems():
+        bibt += '\\item[\\pageref{'+tup[1]+'}] '+cit+'\n'
+    bibt += '\n \\end{itemize}'
+    return bibt
+
+
 tree = ET.parse('abstracts.xml')
 submissions = tree.getroot()
 abst = {}
@@ -225,7 +252,7 @@ for submission in submissions:
 
         titl = format_title(submission[0].text)
         dc = dc.replace('---ti---', titl)
-        dc = dc.replace('---te---', sanitize(format_text(submission[2].text)))
+        dc = dc.replace('---te---', format_text(submission[2].text, titl))
 
         names = [(sanitize(entry[0].text), lower_dut(entry[1].text)) for entry in submission[4]]
         afils = [sanitize(entry[3].text) for entry in submission[4]]
@@ -238,6 +265,8 @@ for submission in submissions:
 with open('./tex/bos_test.tex', 'r') as i:
     o = open('./tex/bos_o.tex', 'w')
     i = i.read()
-    o.write(i.replace('% abstracts', '\n'.join([v for v in OrderedDict(sorted(abst.items(),
-                      key=lambda t: t[0])).itervalues()]).encode("utf-8")))
+    i = i.replace('% abstracts', '\n'.join([v for v in OrderedDict(sorted(abst.items(),
+                      key=lambda t: t[0])).itervalues()]).encode("utf-8"))
+    i = i.replace('% refl', get_refs().encode("utf-8"))
+    o.write(i)
     o.close()
