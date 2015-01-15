71d4b4457c563818ea7eadf8671c23eaaea1e53e
fazzeh <c.emmery@outlook.com>
2015-01-15 11:55:10 +0100

    global sanitization, added comments

--- main.py
+++ main.py
@@ -39,80 +39,92 @@ inil = {'Van ': 'van ', 'Den ': 'den ', 'Der ': 'der ', 'De ': 'de ',
         'Het ': 'het ', "'T ": "'t ", 'Des ': 'des ', 'Op ': 'op '}
 titl = ['and', 'the', 'a', 'for', 'in']
 
-cust = {'Veronique Hoste':                  u"Hoste, V\u00E9ronique",
-        'Quynh Ngoc Thi Do':                u'Do, Quynh',
-        'Antal van den Bosch':              u'van Den Bosch, Antal',
-        'Antal van Den Bosch':              u'van Den Bosch, Antal'}
-
-
 def sanitize(texs, istex=None):
     """
-    This function is used to escape any colliding characters with
-    LaTeX, correct stupid unicode characters, and do a clean-up on
-    some other user/system errors.
+    Escapes any colliding characters with LaTeX, correct stupid unicode
+    characters, and do a clean-up on some other user/system errors.
+
     :param texs: str abstract (or any) text in unicode
     :return: str sanitized text ready for LaTeX compiling
     """
-    # LaTeX collides
-    texs = texs.replace(" ''", " ``")
-    texs = texs.replace(" '", " `")
-    texs = texs.replace("&", "\&")
-    texs = texs.replace("~", "\~")
-    texs = texs.replace("\%", "%")
-    texs = texs.replace("%", "\%")
-    texs = texs.replace("_", "\_")
-    # crappy unicode stuff
-    #texs = texs.replace('–', '-')
-    texs = texs.replace(u'\u00A0', " ")  # screw you mac
-    texs = texs.replace(u'\u00AD', "")   # and some more
-    # user errors
-    texs = texs.replace('htttp', 'http')
-    # system errors
-    texs = texs.replace("&amp;", "&")
-    texs = texs.replace("amp;", '')
-    # text only stuff
-    if not istex:
+            # names
+    cust = {'Veronique':                                    u'V\u00E9ronique',
+            'Ngoc Thi Do':                                  u'Do',
+            'Antal van den Bosch':                          u'Antal van Den Bosch',
+            # afils
+            'Language and Translation Technology Team, ':   u'',
+            # user errors
+            'CLARIN for Linguists LOT ':                    u'CLARIN%20for%20Linguists%20LOT%20',
+            '2014 Summerschool Illustration ':              u'2014%20Summerschool%20Illustration%20',
+            '1 2014-06-22':                                 u'1%202014-06-22',
+            'htttp':                                        u'http',
+            # LaTeX collides
+            '{':                                            u'\{',
+            '}':                                            u'\}',
+            " ''":                                          u" ``",
+            " '":                                           u" `",
+            "&":                                            u"\&",
+            "~":                                            u"\~",
+            "\%":                                           u"%",
+            "%":                                            u"\%",  # wtf?
+            "_":                                            u"\_",
+            # crappy mac unicode stuff
+            u'\u00A0':                                      u" ",
+            u'\u00AD':                                      u"",
+            # system errors
+            "&amp;":                                        u"&",
+            "amp;":                                         u''
+            }
+    
+    for orig, repl in cust.iteritems():
+        texs = texs.replace(orig, repl)
+    if not istex:  # text only
         texs = texs.replace('\n', ' ')
-        texs = texs.replace("}", "\}")
-        texs = texs.replace("{", "\{")
         texs = token_url(texs, True)
     return texs
 
 
 def token_url(text, fn=False):
     """
-    This function is used to recognize URLs in text and format them
-    so that they will be placed in a footnote underneath the abstract.
-    It also makes sure that certain stylistic clashes are omitted, for
-    example a colon before the footnote number.
+    Recognizes URLs in text and format them so that they will be
+    placed in a footnote underneath the abstract. It also makes sure
+    that certain stylistic clashes are omitted, for example a colon
+     before the footnote number.
+
     :param text: str unicode text
     :return: str with footnoted URLs
     """
-    # TODO: smarter handling of foonote
-    urls = findall(' ?(?:http|ftp|https):\/\/[a-z./0-9%].*(?:\.[a-z]{2,5}|\/)(?: |\n|\.|\)|,|$)', text)
+    # TODO: smarter handling of footnote
+    urls = findall(' ?(?:(?:http|ftp|https):\/\/[a-z./0-9%]|\/?www).*(?:\.[a-z]{2,5}|\/)(?: |\n|\.|\)|,|$)', text)
     if urls:
         for u in urls:
             lm = u[len(u)-1:]  # try to trim stuff before the URL
             u = u[:-1] if (lm == '.' or lm == ')' or lm == ',') else u  # trim
             text = text.replace(u, ("\\footnote{" if fn else '') + " \\url{"+u+"}" + ("}" if fn else ''))  # insert footnote
-    burls = findall('(?:,|\.)' + ('\\\\footnote\{' if fn else '') + ' \\\\url\{.*\}' + ('\}' if fn else ''), text)
-    if burls:  # if , or . before footnote, switch them
-        for bu in burls:
-            text = text.replace(bu, bu[1:]+bu[0])
+    if fn:
+        burls = findall('(?:,|\.)' + ('\\\\footnote\{' if fn else '') + ' \\\\url\{.*\}' + ('\}' if fn else ''), text)
+        if burls:  # if , or . before footnote, switch them
+            for bu in burls:
+                text = text.replace(bu, bu[1:]+bu[0])
     return text
 
 
-def check_double_ref(text):
-    textx = text.replace("],", "] ,")
-    textx = textx.replace("])", "] )")
-    textix = textx.lower().split()
-    textii = deepcopy(textix)
-    textii.remove('[1]')
-    return textii
-
-
 def format_ref(refs, label):
+    """
+    Given a string with references able to be splitted by newline,
+    adds to global bib a tuple with a unique id, and the label it
+    will use to refer to the page it's mentioned on, as well as a
+    cleaned out version of the references. Custom part had to be
+    implemented because one of the references was split up by
+    newlines within references.
+
+    :param refs: str unicode text snippet with \n splittable refs
+    :param label: here sec:title is used to pageref to the abstract
+    :return: None (adds to global bib)
+    """
+    global bib
     refs = refs.split('\n')
+    # TODO: find a way to handle the custom line better
     refs = list(' '.join(refs)) if 'Ramage' in ' '.join(refs) else refs  # custom!
     for n in refs:
         if len(n) > 10:
@@ -122,6 +134,15 @@ def format_ref(refs, label):
 
 
 def format_text(text, title):
+    """
+    Finds the boundary between the list of references and the
+    abstract text, will provide a label for the abstract, and
+    pipe the found references towards format_ref function.
+
+    :param text: the abstracts text, including refs
+    :param title: the title of the abstract
+    :return: str unicode with the abstract text and label, no refs
+    """
     ref = findall(r'\n(?:[Rr]eference)|(?:REFERENCE)[sS]?[:]?', text)
     brf = findall(r'\n\[[0-9]\]', text)
     label = 'tit:' + str(hash(title))
@@ -133,14 +154,18 @@ def format_text(text, title):
 
 
 def format_toc(tit, name_l):
-    # TODO: these are the ugliest two one-liners in existance, please fix them to something
-    # readable; first converts tuples to strings, checks in dict, why not have either tuples
-    # in dict or just join to string and then do the second function for adding, because
-    # that converts to a string anyway. First one breaks everything, great.
-    # name_l = [(i.split()[0], i.split()[1]) for i in [n[0]+' '+n[1]
-    # if n[0]+' '+n[1] not in cust else cust[n[0]+' '+n[1]] for n in name_l]]
-    aut = ', '.join([('\\newline ' if (name_l.index(n) == 4 and len(', '.join([n[0]+' '+n[1] for n in name_l[:3]])) < 72) else '') + n[0]+' '+n[1] for n in name_l])
-    # --------------------------------------------------------------------------------------
+    """
+    Accepts title and list of tuples with names from the authors
+    of the abstract, and will convert these into a formatted unicode
+    LaTeX toc entry.
+
+    :param tit: str unicode abstract title (no linebreaks)
+    :param name_l: list str with authors
+    :return: str unicode ToC entry for the abstract
+    """
+    # TODO: refactor this function to look more like the one on fazzeh.github.io
+    aut = ', '.join([('\\newline ' if (name_l.index(n) == 4 and len(', '.join([n[0]+' '+n[1] for n in name_l[:3]]))
+                                       < 72) else '') + n[0]+' '+n[1] for n in name_l])
     aut = aut.split(' \\newline')
     tit = sanitize(tit.replace('\\\\ ', ''))
     tit = "\\addcontentsline{toc}{section}{\\emph{" + tit + "}} \n" + \
@@ -154,9 +179,11 @@ def format_toc(tit, name_l):
 
 def check_prio(tl):
     """
+    Checks if there is a character in the title which has priority
+    as being a split marker.
 
-    :param tl:
-    :return:
+    :param tl: str unicode title of the abstract
+    :return: int index of the priority if exists, else None
     """
     mark = [':' if ':' in i else False or '(' if '(' in i else False for i in tl]
     if '(' in mark:
@@ -168,8 +195,13 @@ def check_prio(tl):
 
 
 def format_title(tit):
+    """
+
+    :param tit:
+    :return:
+    """
     global titl
-    # TODO: { } is not handled yet
+    tit = sanitize(tit)
     if 62 < len(tit) < 96:
         tl = tit.split()
         tl.insert(len(tl)/2 if not check_prio(tl) else check_prio(tl), '\\\\')
@@ -192,12 +224,8 @@ def lower_dut(surename):
 
 
 def format_name(name):
-    global cust
     st_name = name[0]+' '+name[1]
-    if st_name not in cust:
-        return st_name + " \\index{" + name[1] + ", " + name[0] + "} "
-    else:
-        return st_name + " \\index{" + cust[st_name] + "} "
+    return st_name + " \\index{" + name[1] + ", " + name[0] + "} "
 
 
 def format_table(namel, afil, maill):
@@ -232,41 +260,46 @@ def format_table(namel, afil, maill):
 def get_refs():
     global bib
     bib = OrderedDict(sorted(bib.items(), key=lambda (k, v): v))
-    bibt = '\\chapter{Bibliography} \n\\begin{itemize} \n'
+    bibt = '\\chapter*{Bibliography} \n\\begin{itemize} \n'
     for tup, cit in bib.iteritems():
         bibt += '\\item[\\pageref{'+tup[1]+'}] '+cit+'\n'
     bibt += '\n \\end{itemize}'
     return bibt
 
 
-tree = ET.parse('abstracts.xml')
-submissions = tree.getroot()
-abst = {}
+def main():
 
-for submission in submissions:
-    if not 'REJECT' in submission[3].text:
-        dc = str(tex)
+    tree = ET.parse('abstracts.xml')
+    submissions = tree.getroot()
+    abst = {}
 
-        sd = submission.attrib['id']
-        kw = [k.text for k in submission[1]]
+    for submission in submissions:
+        if not 'REJECT' in submission[3].text:
+            dc = str(tex)
 
-        titl = format_title(submission[0].text)
-        dc = dc.replace('---ti---', titl)
-        dc = dc.replace('---te---', format_text(submission[2].text, titl))
+            sd = submission.attrib['id']
+            kw = [k.text for k in submission[1]]
 
-        names = [(sanitize(entry[0].text), lower_dut(entry[1].text)) for entry in submission[4]]
-        afils = [sanitize(entry[3].text) for entry in submission[4]]
-        mails = [(sanitize(entry[2].text) if entry[2].text else '') for entry in submission[4]]
+            title = format_title(submission[0].text)
+            dc = dc.replace('---ti---', title)
+            dc = dc.replace('---te---', format_text(submission[2].text, title))
 
-        dc = dc.replace('---tr---', format_toc(titl, names))
-        dc = dc.replace('---na---', format_table(names, afils, mails))
-        abst[submission[0].text] = dc
+            names = [(sanitize(entry[0].text), lower_dut(entry[1].text)) for entry in submission[4]]
+            afils = [sanitize(entry[3].text) for entry in submission[4]]
+            mails = [(sanitize(entry[2].text) if entry[2].text else '') for entry in submission[4]]
 
-with open('./tex/bos_test.tex', 'r') as i:
-    o = open('./tex/bos_o.tex', 'w')
-    i = i.read()
-    i = i.replace('% abstracts', '\n'.join([v for v in OrderedDict(sorted(abst.items(),
-                      key=lambda t: t[0])).itervalues()]).encode("utf-8"))
-    i = i.replace('% refl', get_refs().encode("utf-8"))
-    o.write(i)
-    o.close()
+            dc = dc.replace('---tr---', format_toc(title, names))
+            dc = dc.replace('---na---', format_table(names, afils, mails))
+            abst[submission[0].text] = dc
+
+    with open('./tex/bos_test.tex', 'r') as i:
+        o = open('./tex/bos_o.tex', 'w')
+        i = i.read()
+        i = i.replace('% abstracts', '\n'.join([v for v in OrderedDict(sorted(abst.items(),
+                          key=lambda t: t[0])).itervalues()]).encode("utf-8"))
+        i = i.replace('% refl', get_refs().encode("utf-8"))
+        o.write(i)
+        o.close()
+
+if __name__ == '__main__':
+    main()
